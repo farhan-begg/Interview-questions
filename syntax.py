a1839cb7c8ab46eed5c2c7c2e3b06764b9db8cc1
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-04-12 11:19:46 -0700

    first commit from google docs to editor

---
+++ syntax.py
@@ -0,0 +1,21 @@
+Given a list of n numbers, determine if it contains any duplicate numbers.
+If given n list of numbers check if there are duplicate numbers
+Def chck_dup(arr, size):
+    For i in range(0, size):
+        For j in range{i + 1, size):
+            If arr[i] == [j]:
+                print(arr[i], end= ‘ ‘)
+
+Arr = [1, 2, 3, 4, 5, 2, 1]
+Arr_size = len(arr)
+printRepeating(arr, arr_size)
+
+
+Find the longest substring of unique letters in a given string of n letters.
+No_of_char = 256
+Def unique+substring(string):
+    Word = len(string)
+    Cur_len = 1
+    Max_len = 1
+    Prev_index = 0
+    I = 0
